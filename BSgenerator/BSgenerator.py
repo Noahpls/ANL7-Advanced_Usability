a11b31ad6eec60e63df60e119561f438e729767f
noahplazier <noahplz@hotmail.nl>
2018-05-07 17:15:39 +0200

    You done now

--- BSgenerator/BSgenerator.py
+++ BSgenerator/BSgenerator.py
@@ -1,20 +1,20 @@
 from random import randint
 
-strings = [ 
+strings = [
 	["Het proces ", "De factor mens ", "Het management ", "De communicatie ", "De kerncompetentie ", "Human capital ", "De organisatie-ontwikkeling ", "De missie ", "Kennismanagement ", "De eerste aanzet "],
 	["moet meerwaarde leveren bij ", "stelt eisen aan ", "dient te faciliteren bij ", "is uitgangspunt bij ", "is onlosmakelijk verbonden met ", "schept voorwaarden voor ", "dient te focussen op ", "stuurt ", "hangt nauw samen met ", "moet een opstap bieden voor "],
 	["de implementatie van ", "de terugkoppeling van ", "het aftimmeren van ", "het aansturen van ", "de ontwikkeling van ", "de flexibilisering van ", "de integratie van ", "de inventarisatie van ", "de definitie van ", "de insteek van "],
-	["complexe ","optimale ","in elkaar grijpende ","eenduidige ","onderling afhankelijke ","structurele ","pro-actieve ","resultaatgerichte ","efficiënte ","consistente "],
-	["supply chain processen ","business architecture ","mijlpalen ","targets ","business units ","organisatie-onderdelen ","scenario's ","best practices ","business models ","conceptplannen "],
-	["waarbij het belang van ","waarbij de feedback van ","waarbij het kader voor ","waarbij afstemming met ","waarbij de structuur van ","waarbij de synergie met ","waarbij de interface met ","waarbij input van ","waarbij commitment van ","waarbij klankborden met "],
-	["strategisch beleid ","de taskforce ","de communicatie ","de werkgroepen ","new business development ","de systeemintegratie ","de markt ","de stakeholders ","het management ","de projectorganisatie "],
-	["moet uitkristalleren.","voorop staat.","wordt aangestuurd.","leading is.","toegevoegde waarde levert.","win-win situaties kan veroorzaken.","moet worden gemanaged.","voldoende draagvlak heeft.","doorslaggevend is.","cruciaal is."]
+	["complexe ", "optimale ", "in elkaar grijpende ", "eenduidige ", "onderling afhankelijke ", "structurele ", "pro-actieve ", "resultaatgerichte ", "efficiënte ", "consistente "],
+	["supply chain processen ", "business architecture ", "mijlpalen ", "targets ", "business units ", "organisatie-onderdelen ", "scenario's ", "best practices ", "business models ", "conceptplannen "],
+	["waarbij het belang van ", "waarbij de feedback van ", "waarbij het kader voor ", "waarbij afstemming met ", "waarbij de structuur van ", "waarbij de synergie met ", "waarbij de interface met ", "waarbij input van ", "waarbij commitment van ", "waarbij klankborden met "],
+	["strategisch beleid ", "de taskforce ", "de communicatie ", "de werkgroepen ", "new business development ", "de systeemintegratie ", "de markt ", "de stakeholders ", "het management ", "de projectorganisatie "],
+	["moet uitkristalleren.", "voorop staat.", "wordt aangestuurd.", "leading is.", "toegevoegde waarde levert.", "win-win situaties kan veroorzaken.", "moet worden gemanaged.", "voldoende draagvlak heeft.", "doorslaggevend is.", "cruciaal is."]
 	]
 
 res = ""
 
 for str in strings:
-  x = randint(0, (len(str)-1) )
+  x = randint(0, (len(str)-1))
   res += str[x]
 
 print(res)
