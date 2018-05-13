2f15a03ec326f43ace13e0e2328d1826ba81f89f
noahplazier <noahplz@hotmail.nl>
2018-05-13 16:49:09 +0200

    Studievoolichtingsbot beterder

--- StudievoorlichtingsBot/StudievoorlichtingsBot.py
+++ StudievoorlichtingsBot/StudievoorlichtingsBot.py
@@ -1,30 +1,55 @@
 import re
 import os
+from dictionary import dictionary
 clear = lambda:os.system('cls')
 
-questionStr = "Vraag? "
-
-keywords = ["informatie", "xd", "lol", "endchat"]
+botName = "BOT"
+userName = "USER"
 chatHistory = []
 
-def askQuestion():
+def askQuestion(questionStr, followUpQuestion=[]):
   while True:
-    answer = input(questionStr)
-    for keyword in keywords:
+    formattedQuestion  = formatText(botName, questionStr, True)
+    answer = input(formattedQuestion + "\n" + formatText(userName, "", True))
+    formattedAnswer = formatText(userName, answer, True)
+    chatHistory.append(formattedAnswer)
+    chatHistory.append(formattedQuestion)
+
+    for keyword in list(dictionary.keys()):
       if bool(re.search(keyword, answer.lower())):
-        chatHistory.append(answer)
         break
     else:
-      print("Dit is geen keyword")
+      questionStr = "Ik begrijp niet wat u zegt"
+      chatHistory.append(formatText(botName, questionStr, True))
       continue
     break
   return keyword
 
+def showChatHistory():
+  print("Chat:")
+  for line in chatHistory:
+    print(line)
+
+def formatText(name, text, ret=False):
+  formattedText = '{:<4s} {:<1s}'.format(name, "| " + text)
+  if ret == True:
+    return (formattedText)
+  else:
+    print(formattedText)
+  
+
 def main():
-  clear()
-  askQuestion()
-  print("\nChat history:")
-  for line in chatHistory.__reversed__():
-    print(line + "\n")
+  questionStr = "Waar kan ik u mee helpen? "
+  userInput = ""
+  while True:
+    showChatHistory()
+    clear()
+    userInput = askQuestion(questionStr)
+    if userInput == "endchat":
+      clear()
+      showChatHistory()
+      break
+    else:
+      questionStr = dictionary[userInput]
 
 main()
---
+++ StudievoorlichtingsBot/dictionary.py
@@ -0,0 +1,12 @@
+dictionary = {
+		"informatie" : """We leiden je op tot software engineer. Je gaat aan de slag met het ontwikkelen van software voor diverse toepassingen. Je bent in staat complexe ICT-systemen te analyseren, ontwerpen en implementeren.
+
+Software is overal om ons heen te vinden. Jij bent de specialist in het maken van grote en complexe software informatiesystemen die snel, efficiënt en veilig werken. Zo kun je denken aan het ontwikkelen van een functionele app, maar ook aan het analyseren van grote hoeveelheden data van social media. Bij het ontwikkelen van deze systemen kom je in aanraking met verschillende programmeertalen. Zo leer je in het eerste jaar programmeren in Python en Java/C#. Vanaf het tweede jaar kies je zelf welke programmeertaal je wilt gebruiken. Naast het programmeren en ontwikkelen van verschillende toepassingen leer je ook samenwerken.
+
+Na het volgen van de opleiding Informatica ben je breed inzetbaar binnen het vakgebied. Vanuit functies als programmeur of applicatieontwikkelaar kun je verder groeien binnen de ICT.
+
+Ben jij geïnteresseerd in nieuwe techno logische ontwikkelingen, nieuwsgierig, een beetje eigenzinnig en een doorzetter, dan is de opleiding Informatica iets voor jou!""",
+		"endchat" : "waow",
+		"slecht" : "Informatica is niks voor jou, shoo shoo"
+	}
+ 
